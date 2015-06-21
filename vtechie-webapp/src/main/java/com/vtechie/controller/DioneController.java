1deb3f3c5d4d36c82c4f420c376b144b80fa40f4
Marimuthu Vellingiri <marimuthu_vellingiri@mastercard.com>
2015-06-21 12:04:18 -0700

    D3 - Offer Charts

--- vtechie-webapp/src/main/java/com/vtechie/controller/DioneController.java
+++ vtechie-webapp/src/main/java/com/vtechie/controller/DioneController.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DioneController {
@@ -13,12 +15,29 @@ public class DioneController {
 	private final static Logger logger = LoggerFactory.getLogger(DioneController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
+	public String home(ModelMap model) {
  
 		logger.debug("Inside controller");
  
-		// Spring uses InternalResourceViewResolver and return back index.jsp
-		return "home";
+		return "regionChart";
+ 
+	}
+
+	@RequestMapping(value = "/getregionoffers", method = RequestMethod.GET)
+	public String getRegionOffers(ModelMap model) {
+ 
+		logger.debug("Inside controller");
+		
+		return "regionChart";
+ 
+	}
+	
+	@RequestMapping(value = "/getofferbyage/{region}", method = RequestMethod.GET)
+	public String getOfferInfoByAge(ModelMap model,@PathVariable("region") String region) {
+ 
+		logger.debug("Inside controller");
+		model.addAttribute("region", region);
+		return "groupChart";
  
 	}
 
--- vtechie-webapp/src/main/webapp/WEB-INF/pages/home.jsp
+++ vtechie-webapp/src/main/webapp/WEB-INF/pages/home.jsp
@@ -4,13 +4,13 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
     <title>D3 Hello World</title>
-    <script type="text/javascript" src="http://mbostock.github.com/d3/d3.js?2.4.5"></script>
+    <script type="text/javascript" src="resources/lib/d3.v3.min.js"></script>
 
   </head>
   <body>
     <script type="text/javascript">
 
-		d3.csv("/resources/data.csv",function(data){
+		d3.csv("resources/data.csv",function(data){
 		
 			var canvas=d3.select("body").append("svg")
 				.attr("width",500)
@@ -20,7 +20,7 @@
 				.data(data)
 				.enter()
 					.append("rect")
-					.attr("width",function (d){return d*age * 10;})
+					.attr("width",function (d){return d.age * 10;})
 					.attr("height",48)
 					.attr("y",function (d,i){return i* 50;})
 					.attr("fill","blue");
